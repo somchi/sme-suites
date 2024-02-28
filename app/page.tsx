1a1eccf370034f768fdeb5bef45e187f8549317c
olubori <daveholuborhee@gmail.com>
2024-02-28 20:25:02 +0100

    Add modules to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import DeployButton from "../components/DeployButton";
-import InvoiceButton from "../components/InvoiceButton";
 
-import AuthButton from "../components/AuthButton";
+// import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/SignUpUserSteps";
 import Header from "@/components/Header";
 import { cookies } from "next/headers";
+import LandingModules from "@/components/LandingModules";
 
 export default async function Index() {
   const cookieStore = cookies();
@@ -29,13 +29,12 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-
-          <InvoiceButton />
+          {/* {isSupabaseConnected && <AuthButton />} */}
         </div>
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+        <LandingModules />
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
