e67da8208194795641863e190132a1edcf318e35
olubori <daveholuborhee@gmail.com>
2024-02-29 12:41:04 +0100

    fix server rendering error by upgrading to next14

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import DeployButton from "../components/DeployButton";
 
-// import AuthButton from "../components/AuthButton";
+import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/SignUpUserSteps";
@@ -29,7 +29,7 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
-          {/* {isSupabaseConnected && <AuthButton />} */}
+          {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
 
